3a74ea58f39cba6b3b3478bc10a4b44384bc8134
ChrisRoland <chrisebuberoland@gmail.com>
2025-02-17 14:27:34 +0100

    formatting

--- script.js
+++ script.js
@@ -1,94 +1,94 @@
-const quiz = document.getElementById('quiz')
-const answerEls = document.querySelectorAll('.answer')
-const questionEl = document.getElementById('question')
-const aText = document.getElementById('a-text')
-const bText = document.getElementById('b-text')
-const cText = document.getElementById('c-text')
-const dText = document.getElementById('d-text')
-const submitBtn = document.getElementById('submit')
+const quiz = document.getElementById("quiz");
+const answerEls = document.querySelectorAll(".answer");
+const questionEl = document.getElementById("question");
+const aText = document.getElementById("a-text");
+const bText = document.getElementById("b-text");
+const cText = document.getElementById("c-text");
+const dText = document.getElementById("d-text");
+const submitBtn = document.getElementById("submit");
 
 const quizData = [
-    {
-        question: "What does CSS stand for?",
-        a: "Central Style Sheets",
-        b: "Cascading Style Sheets",
-        c: "Cascading Simple Sheets",
-        d: "Cars SUVs Sailboats",
-        correct: "b",
-    },
-    {
-        question: "Which language runs in a web browser?",
-        a: "Java",
-        b: "C",
-        c: "Python",
-        d: "JavaScript",
-        correct: "d",
-    },
-    {
-        question: "What does HTML stand for?",
-        a: "Hypertext Markup Language",
-        b: "Hypertext Markdown Language",
-        c: "Hyperloop Machine Language",
-        d: "Helicopters Terminals Motorboats Lamborginis",
-        correct: "a",
-    },
-    {
-        question: "What year was JavaScript launched?",
-        a: "1996",
-        b: "1995",
-        c: "1994",
-        d: "none of the above",
-        correct: "b",
-    },
+  {
+    question: "What does CSS stand for?",
+    a: "Central Style Sheets",
+    b: "Cascading Style Sheets",
+    c: "Cascading Simple Sheets",
+    d: "Cars SUVs Sailboats",
+    correct: "b",
+  },
+  {
+    question: "Which language runs in a web browser?",
+    a: "Java",
+    b: "C",
+    c: "Python",
+    d: "JavaScript",
+    correct: "d",
+  },
+  {
+    question: "What does HTML stand for?",
+    a: "Hypertext Markup Language",
+    b: "Hypertext Markdown Language",
+    c: "Hyperloop Machine Language",
+    d: "Helicopters Terminals Motorboats Lamborginis",
+    correct: "a",
+  },
+  {
+    question: "What year was JavaScript launched?",
+    a: "1996",
+    b: "1995",
+    c: "1994",
+    d: "none of the above",
+    correct: "b",
+  },
 ];
 
-let currentQuestion = 0
-let score = 0
+let currentQuestion = 0;
+let score = 0;
 
-startQuiz()
+startQuiz();
 
 function startQuiz() {
-    unselectAnswer()
+  unselectAnswer();
 
-    const currentQuestionData = quizData[currentQuestion]
+  const currentQuestionData = quizData[currentQuestion];
 
-    questionEl.innerText = currentQuestionData.question
-    aText.innerText = currentQuestionData.a
-    bText.innerText = currentQuestionData.b
-    cText.innerText = currentQuestionData.c
-    dText.innerText = currentQuestionData.d 
+  questionEl.innerText = currentQuestionData.question;
+  aText.innerText = currentQuestionData.a;
+  bText.innerText = currentQuestionData.b;
+  cText.innerText = currentQuestionData.c;
+  dText.innerText = currentQuestionData.d;
 }
 
 function unselectAnswer() {
-    answerEls.forEach(answerEl => answerEl.checked = false)
+  answerEls.forEach((answerEl) => (answerEl.checked = false));
 }
 
 function getSelected() {
-    let answer
+  let answer;
 
-    answerEls.forEach(answerEl => {
-        if(answerEl.checked) {
-            answer = answerEl.id
-        }
-    })
+  answerEls.forEach((answerEl) => {
+    if (answerEl.checked) {
+      answer = answerEl.id;
+    }
+  });
 
-    return answer
+  return answer;
 }
 
 submitBtn.addEventListener("click", () => {
-    const answer = getSelected()
+  const answer = getSelected();
 
-    if(answer) {
-        if(answer === quizData[currentQuestion].correct) {
-            score++
-        }
+  if (answer) {
+    if (answer === quizData[currentQuestion].correct) {
+      score++;
+    }
 
-        currentQuestion++
+    currentQuestion++;
 
-        if(currentQuestion < quizData.length) {
-            startQuiz()
-        } else {
-            quiz.innerHTML = `<h2>Your score is ${score}/${quizData.length}</h2>
+    if (currentQuestion < quizData.length) {
+      startQuiz();
+    } else {
+      quiz.innerHTML = `<h2>Your score is ${score}/${quizData.length}</h2>
             <div class="review" id="review">
                 <ol>
                     <li>${quizData[0].question} <br> <span style="font-weight: bold;">Ans: ${quizData[0].correct}</span></li>
@@ -101,17 +101,16 @@ submitBtn.addEventListener("click", () => {
             <div class="btns">
             <button class="restart" onclick="location.reload()">Restart</button>
             <button class="restart" id="review-btn" onclick="review()">Review answers</button>
-            </btn>`
-        }
+            </btn>`;
     }
-})
+  }
+});
 
 function review() {
-    const review = document.getElementById("review")
-    const reviewBtn = document.getElementById("review-btn")
+  const review = document.getElementById("review");
+  const reviewBtn = document.getElementById("review-btn");
 
-    reviewBtn.addEventListener("click", () => {
-        review.style.display = "block"}
-    )
+  reviewBtn.addEventListener("click", () => {
+    review.style.display = "block";
+  });
 }
-
